bac4a24a89ccd8df93dedf6e7622bcb1b95714f0
Lorenzo HOSTEIN <lhostein@cfai-formation.fr>
2018-10-22 09:16:49 +0200

    Finalisation du lancé et redimensionnement fenêtre

--- MonopolyVS/MonopolyVS/MainWindow.xaml.cs
+++ MonopolyVS/MonopolyVS/MainWindow.xaml.cs
@@ -31,7 +31,11 @@ namespace MonopolyVS
         private void ButtonLanceDes_Click(object sender, RoutedEventArgs e)
         {
             Des.Lancer();
-            TextBlockConsole.Text = Convert.ToString(Des.RecuperePremier()) + "\n" + Convert.ToString(Des.RecupereDeuxieme());
+            TextBoxConsole.AppendText("\nLe premier dé affiche un " + Convert.ToString(Des.RecuperePremier()) + "\n");
+            TextBoxConsole.AppendText("Le deuxième dé affiche un " + Convert.ToString(Des.RecupereDeuxieme()) + "\n");
+            if (Des.EstDouble() == true)
+                TextBoxConsole.AppendText("Doublé\n");
+            TextBoxConsole.ScrollToEnd();
         }
     }
 }
