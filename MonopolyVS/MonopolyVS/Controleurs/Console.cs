907900dbd2628be72e0f655c372b7faf9dd812a6
Lorenzo HOSTEIN <lhostein@cfai-formation.fr>
2019-03-31 10:19:03 +0200

    feat : création d'un controleur Console

---
+++ MonopolyVS/MonopolyVS/Controleurs/Console.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MonopolyVS.Controleurs
+{
+    class Console
+    {
+        #region
+        private TextBox Afficheur;
+        #endregion
+        
+        #region CONSTRUCTEURS
+        public Console()
+        {
+
+        }
+
+        public Console(TextBox console)
+        {
+
+        }
+        #endregion
+
+        #region METHODES
+        public void Print(string aAfficher)
+        {
+            Afficheur.AppendText(aAfficher);
+            Afficheur.ScrollToEnd();
+        }
+        #endregion
+    }
+}
